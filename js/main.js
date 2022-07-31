416f3f66381f7bcd5ca8983ba94a70a663d3564a
juandelcerro <juanignaciodelcerro@gmail.com>
2022-07-31 15:04:12 -0300

    Agregando JS al sitio

---
+++ js/main.js
@@ -0,0 +1,19 @@
+class Producto {
+    constructor(nombre, precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+}
+
+const producto1 = new Producto("Almohadon y Antifaz", 500);
+const producto2 = new Producto("Almohadones", 500);
+const producto3 = new Producto("Conejo de Ceramica", 700);
+const producto4 = new Producto("Conejo con Luz", 1000);
+const producto5 = new Producto("Conejos Tejidos", 500);
+const producto6 = new Producto("Kit Creativo", 1000);
+const producto7 = new Producto("Marineras", 600);
+const producto8 = new Producto("Mochila de Peluche", 1200);
+const producto9 = new Producto("Mochila con Brillo", 1200);
+
+const productos = [producto1, producto2, producto3, producto4, producto5, producto6, producto7, producto8, producto9];
+
