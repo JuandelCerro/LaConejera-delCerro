2b931240685cece56128e1f0c41e666d97d25047
juandelcerro <juanignaciodelcerro@gmail.com>
2022-08-25 19:47:03 -0300

    Incorporando fetch

---
+++ js/carrito.js
@@ -0,0 +1,7 @@
+contadorCarrito = localStorage.getItem('contadorCarrito');
+document.getElementById("carritoContador").innerHTML = contadorCarrito;
+
+if (contadorCarrito == null) {
+    totalCarrito = 0;
+    document.getElementById("carritoContador").innerHTML = totalCarrito;
+}
