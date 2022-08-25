da1d54fbd4539370168100358467af0dd3fd95d2
juandelcerro <juanignaciodelcerro@gmail.com>
2022-08-25 18:49:44 -0300

    Usando fetch para traer los datos de un JSON

--- js/main.js
+++ js/main.js
@@ -1,34 +1,37 @@
+ function cargarDatos () {
     fetch('./json/data.json')
         .then((res) => res.json())
-        .then((data) => console.log(data));
+        .then((data) => localStorage.setItem('dataProductos',JSON.stringify(data)));
+ }
+
+cargarDatos();
 
 
-class Producto {
-    constructor(id, nombre, precio) {
-        this.id = id;
-        this.nombre = nombre;
-        this.precio = precio;
-    }
-}
+// class Producto {
+//     constructor(id, nombre, precio) {
+//         this.id = id;
+//         this.nombre = nombre;
+//         this.precio = precio;
+//     }
+// }
 
-const listaProductos = [];
+// const listaProductos = [];
 
-
-listaProductos.push(new Producto(1, "Almohadon y Antifaz", 500));
-listaProductos.push(new Producto(2, "Almohadones", 500));
-listaProductos.push(new Producto(3, "Conejo de Ceramica", 700));
-listaProductos.push(new Producto(4, "Conejo con Luz", 1000));
-listaProductos.push(new Producto(5, "Conejos Tejidos", 500));
-listaProductos.push(new Producto(6, "Kit Creativo", 1000));
-listaProductos.push(new Producto(7, "Marineras", 600));
-listaProductos.push(new Producto(8, "Mochila de Peluche", 1200));
-listaProductos.push(new Producto(9, "Mochila con Brillo", 1200));
-listaProductos.push(new Producto(10, "Brownie", 1500));
-listaProductos.push(new Producto(11, "Chessecake", 1500));
-listaProductos.push(new Producto(12, "Chocotorta", 1400));
-listaProductos.push(new Producto(13, "Lemon Pie", 1400));
-listaProductos.push(new Producto(14, "Rogel", 1700));
-listaProductos.push(new Producto(15, "Tarta de Manzana", 1700));
+// listaProductos.push(new Producto(1, "Almohadon y Antifaz", 500));
+// listaProductos.push(new Producto(2, "Almohadones", 500));
+// listaProductos.push(new Producto(3, "Conejo de Ceramica", 700));
+// listaProductos.push(new Producto(4, "Conejo con Luz", 1000));
+// listaProductos.push(new Producto(5, "Conejos Tejidos", 500));
+// listaProductos.push(new Producto(6, "Kit Creativo", 1000));
+// listaProductos.push(new Producto(7, "Marineras", 600));
+// listaProductos.push(new Producto(8, "Mochila de Peluche", 1200));
+// listaProductos.push(new Producto(9, "Mochila con Brillo", 1200));
+// listaProductos.push(new Producto(10, "Brownie", 1500));
+// listaProductos.push(new Producto(11, "Chessecake", 1500));
+// listaProductos.push(new Producto(12, "Chocotorta", 1400));
+// listaProductos.push(new Producto(13, "Lemon Pie", 1400));
+// listaProductos.push(new Producto(14, "Rogel", 1700));
+// listaProductos.push(new Producto(15, "Tarta de Manzana", 1700));
 
 // console.log(listaProductos);
 
@@ -51,7 +54,7 @@ for (const elemento of eventoBotonAgregar) {
         event.preventDefault();
 
         console.log(event.target.id);
-        let producto = listaProductos.find(item => item.id === parseInt(event.target.id));
+        let producto = JSON.parse(localStorage.getItem('dataProductos')).find(item => item.id === parseInt(event.target.id));
         let {id, nombre, precio} = producto; // Desestructuracion producto
 
         productosCarrito.push(new ProductoCarrito(id, nombre, precio)); // Agregando productos al array del carrito
